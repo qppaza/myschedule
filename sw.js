2c1bff56256f3dab7f90b401e5bfe7909beab5a6
qppaza <qppaza@gmail.com>
2026-09-22 08:10:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change VERSION when publishing a new offline bundle.
-const VERSION = 'myschedule-offline-v19';
+const VERSION = 'myschedule-offline-v20';
 const BASE = self.registration.scope;
 const CACHE = VERSION + ':' + BASE;
 const INDEX = new URL('index.html', BASE).href;
